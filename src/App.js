e13215be6d0be04d550e3294f67efa68c6b6e6a6
Jerika Kanz <jerikacapila@gmail.com>
2022-05-30 17:24:49 +0200

    Create getValue() and getResult()

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import { useState } from "react";
 import "./App.css";
 
 export default function App() {
+  const [equation, setEquation] = useState("");
+  const [result, setResult] = useState(0);
+
+  // console.log(eval(equation));
+
+  function getValue(event) {
+    setEquation(event.target.value);
+    setResult(eval(event.target.value));
+  }
+
+  function getResult() {
+    console.log(result);
+  }
+
   return (
     <div className="calculator-container">
-      <input className="calculator-screen" type="text"></input>
+      <input
+        className="calculator-screen"
+        pattern="[0-9]+"
+        type="type"
+        onChange={getValue}
+      ></input>
       <ol className="calculator-buttons">
         <li className="">9</li>
         <li className="">8</li>
@@ -16,7 +36,9 @@ export default function App() {
         <li className="">1</li>
         <li className="">0</li>
         <li className="">.</li>
-        <li className="">=</li>
+        <li className="" onClick={() => getResult()}>
+          =
+        </li>
       </ol>
     </div>
   );
