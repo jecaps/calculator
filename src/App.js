df776923eb106dd73601c21b874299eed219a428
Jerika Kanz <jerikacapila@gmail.com>
2022-05-30 12:24:38 +0200

    Create Calculator Face

--- src/App.js
+++ src/App.js
@@ -2,6 +2,22 @@ import "./App.css";
 
 export default function App() {
   return (
-    <h1 className="text-3xl bg-indigo-500 font-bold underline">Hello world!</h1>
+    <div className="calculator-container">
+      <input className="calculator-screen" type="text"></input>
+      <ol className="calculator-buttons">
+        <li className="">9</li>
+        <li className="">8</li>
+        <li className="">7</li>
+        <li className="">6</li>
+        <li className="">5</li>
+        <li className="">4</li>
+        <li className="">3</li>
+        <li className="">2</li>
+        <li className="">1</li>
+        <li className="">0</li>
+        <li className="">.</li>
+        <li className="">=</li>
+      </ol>
+    </div>
   );
 }
