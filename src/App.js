d3b362b5f17c156c45d21897ba6b1648412a79fe
Jerika Kanz <jerikacapila@gmail.com>
2022-06-02 14:00:53 +0200

    Create btnSelector()

--- src/App.js
+++ src/App.js
@@ -1,103 +1,132 @@
-// import { useState } from "react";
+import { useState } from "react";
 import "./App.css";
 
 export default function App() {
+  const [formula, setFormula] = useState("");
+  const [screenFormula, setScreenFormula] = useState("");
+  const [result, setResult] = useState(0);
+
+  function btnSelector(button) {
+    let btnText = document.querySelector(button).innerHTML;
+    switch (btnText) {
+      case "-":
+        setScreenFormula(`${screenFormula} ${btnText} `);
+        setFormula(`${formula} ${btnText} `);
+        break;
+      case "+":
+        setScreenFormula(`${screenFormula} ${btnText} `);
+        setFormula(`${formula} ${btnText} `);
+        break;
+      case "÷":
+        setScreenFormula(`${screenFormula} ${btnText} `);
+        setFormula(`${formula} / `);
+        break;
+      case "×":
+        setScreenFormula(`${screenFormula} ${btnText} `);
+        setFormula(`${formula} * `);
+        break;
+      case "EXP":
+        setScreenFormula(`${screenFormula} ^ `);
+        setFormula(`${formula} ** `);
+        break;
+      case "AC":
+        setScreenFormula("");
+        setFormula("");
+        setResult(0);
+        break;
+      case "DEL":
+        setScreenFormula(screenFormula.slice(0, -1));
+        setFormula(formula.slice(0, -1));
+        break;
+      default:
+        setScreenFormula(`${screenFormula}${btnText}`);
+        setFormula(`${formula}${btnText}`);
+    }
+  }
+
+  function getResult() {
+    setResult(eval(formula));
+  }
+
   return (
     <div className="calculator-grid grid mt-8 grid-cols-4 grid-rows-7 justify-center">
       <div className="calculator-screen col-span-full bg-screen flex flex-col items-end justify-around p-3 break-all">
-        <div className="equation text-white text-1.5">576465 +</div>
-        <div className="result text-white-pure text-2.5">132135466541</div>
+        <div className="equation text-white text-1.5">{screenFormula}</div>
+        <div className="result text-white-pure text-2.5">{result}</div>
       </div>
-      <button className="span-two ">AC</button>
-      <button className="span-two ">DEL</button>
-      <button className="">(</button>
-      <button className="">)</button>
-      <button className="">exp</button>
-      <button className="">÷</button>
-      <button className="">7</button>
-      <button className="">8</button>
-      <button className="">9</button>
-      <button className="">×</button>
-      <button className="">4</button>
-      <button className="">5</button>
-      <button className="">6</button>
-      <button className="">+</button>
-      <button className="">1</button>
-      <button className="">2</button>
-      <button className="">3</button>
-      <button className="">-</button>
-      <button className="">.</button>
-      <button className="">0</button>
-      <button className="span-two">=</button>
+
+      <button className="span-two clear" onClick={() => btnSelector(".clear")}>
+        AC
+      </button>
+      <button
+        className="span-two delete"
+        onClick={() => btnSelector(".delete")}
+      >
+        DEL
+      </button>
+      <button
+        className="open-parenthesis"
+        onClick={() => btnSelector(".open-parenthesis")}
+      >
+        (
+      </button>
+      <button
+        className="close-parenthesis"
+        onClick={() => btnSelector(".close-parenthesis")}
+      >
+        )
+      </button>
+      <button className="exponent" onClick={() => btnSelector(".exponent")}>
+        EXP
+      </button>
+      <button className="divide" onClick={() => btnSelector(".divide")}>
+        ÷
+      </button>
+      <button className="seven" onClick={() => btnSelector(".seven")}>
+        7
+      </button>
+      <button className="eight" onClick={() => btnSelector(".eight")}>
+        8
+      </button>
+      <button className="nine" onClick={() => btnSelector(".nine")}>
+        9
+      </button>
+      <button className="mutliply" onClick={() => btnSelector(".mutliply")}>
+        ×
+      </button>
+      <button className="four" onClick={() => btnSelector(".four")}>
+        4
+      </button>
+      <button className="five" onClick={() => btnSelector(".five")}>
+        5
+      </button>
+      <button className="six" onClick={() => btnSelector(".six")}>
+        6
+      </button>
+      <button className="add" onClick={() => btnSelector(".add")}>
+        +
+      </button>
+      <button className="one" onClick={() => btnSelector(".one")}>
+        1
+      </button>
+      <button className="two" onClick={() => btnSelector(".two")}>
+        2
+      </button>
+      <button className="three" onClick={() => btnSelector(".three")}>
+        3
+      </button>
+      <button className="minus" onClick={() => btnSelector(".minus")}>
+        -
+      </button>
+      <button className="decimal" onClick={() => btnSelector(".decimal")}>
+        .
+      </button>
+      <button className="zero" onClick={() => btnSelector(".zero")}>
+        0
+      </button>
+      <button className="span-two equals" onClick={() => getResult()}>
+        =
+      </button>
     </div>
   );
 }
-
-// const [equation, setEquation] = useState("");
-// const [result, setResult] = useState(0);
-
-// console.log(equation);
-
-// function getValue(event) {
-//   setEquation(event.target.value);
-//   setResult(eval(event.target.value));
-// }
-
-// function getResult() {
-//   console.log(result);
-// }
-
-// function keySelector() {
-//   let val = document.querySelector(".keys").textContent;
-//   document.getElementsByClassName("calculator-screen").innerHTML = val;
-//   console.log(val);
-// }
-
-// return (
-//   <div className="calculator-container">
-//     <input
-//       className="calculator-screen"
-//       pattern="[0-9]+"
-//       type="type"
-//       onChange={getValue}
-//     ></input>
-//     <ol className="calculator-buttons">
-//       <li className="keys" onClick={keySelector}>
-//         9
-//       </li>
-//       <li className="keys" onClick={keySelector}>
-//         8
-//       </li>
-//       <li className="keys" onClick={keySelector}>
-//         7
-//       </li>
-//       <li className="keys" onClick={keySelector}>
-//         6
-//       </li>
-//       <li className="keys" onClick={keySelector}>
-//         5
-//       </li>
-//       <li className="keys" onClick={keySelector}>
-//         4
-//       </li>
-//       <li className="keys" onClick={keySelector}>
-//         3
-//       </li>
-//       <li className="keys" onClick={keySelector}>
-//         2
-//       </li>
-//       <li className="keys" onClick={keySelector}>
-//         1
-//       </li>
-//       <li className="keys" onClick={keySelector}>
-//         0
-//       </li>
-//       <li className="keys" onClick={keySelector}>
-//         .
-//       </li>
-//       <li className="" onClick={() => getResult()}>
-//         =
-//       </li>
-//     </ol>
-//   </div>
-// );
