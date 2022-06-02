ec73cc71a2f605f419071de99c385965457b69df
Jerika Kanz <jerikacapila@gmail.com>
2022-06-02 19:37:29 +0200

    Create data Object

---
+++ src/data.js
@@ -0,0 +1,88 @@
+const data = [
+  {
+    text: "AC",
+    classname: "span-two clear",
+  },
+  {
+    text: "DEL",
+    classname: "span-two delete",
+  },
+  {
+    text: "(",
+    classname: "open-parenthesis",
+  },
+  {
+    text: ")",
+    classname: "close-parenthesis",
+  },
+  {
+    text: "EXP",
+    classname: "exponent",
+  },
+  {
+    text: "÷",
+    classname: "divide",
+  },
+  {
+    text: "7",
+    classname: "seven",
+  },
+  {
+    text: "8",
+    classname: "eight",
+  },
+  {
+    text: "9",
+    classname: "nine",
+  },
+  {
+    text: "×",
+    classname: "multiply",
+  },
+  {
+    text: "4",
+    classname: "four",
+  },
+  {
+    text: "5",
+    classname: "five",
+  },
+  {
+    text: "6",
+    classname: "six",
+  },
+  {
+    text: "+",
+    classname: "add",
+  },
+  {
+    text: "1",
+    classname: "one",
+  },
+  {
+    text: "2",
+    classname: "two",
+  },
+  {
+    text: "3",
+    classname: "three",
+  },
+  {
+    text: "-",
+    classname: "minus",
+  },
+  {
+    text: ".",
+    classname: "decimal",
+  },
+  {
+    text: "0",
+    classname: "zero",
+  },
+  {
+    text: "=",
+    classname: "span-two equals",
+  },
+];
+
+export default data;
